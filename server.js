eae4c8da07d0c877d036b9f67303dd6d91ddc76e
D <kolosochek@gmail.com>
2022-12-22 16:01:46 +0300

    heroku refactor

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 80;
+const hostname = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 
 //static
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 
-app.listen(PORT, () => {
+app.listen(PORT, hostname, () => {
     console.log(`Express server has been started on port ${PORT}`)
 })
 

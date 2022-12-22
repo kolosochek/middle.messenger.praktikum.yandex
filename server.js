4ad5d134de67698e79e4cb08c9ee3efdd839e91c
D <kolosochek@gmail.com>
2022-12-22 18:13:43 +0300

    heroku refactor

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'index.html'));
+    response.sendFile(path.resolve(path.join(__dirname, 'dist'), 'index.html'));
 });
 app.listen(PORT, () => {
     console.log(`Express server has been started on port ${PORT}`)
